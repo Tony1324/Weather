68fa46b88feaef75b72753fe1558c3fce331404e
Tony1324 <61944932+Tony1324@users.noreply.github.com>
2020-04-23 09:08:07 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+window.addEventListener('load',() => {
+	var temp = 0;
+	var description = 0;
+
+	fetch(`http://api.openweathermap.org/data/2.5/weather?q=boston&appid=2669340c1ffaa498ad124cd7d7bb6eba`)
+		.then(res => res.json())
+		.then(data => {
+			temp = data.main.temp;
+			description = data.weather[0].description;
+			document.getElementById('temp').innerHTML = Math.round((temp-273.1)*10)/10+"°C";
+			document.getElementById('description').innerHTML = description;
+		})
+		
+});
