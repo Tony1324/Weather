30306b248b6c9aa79715ed4ec0251b2f1c36af41
Tony1324 <61944932+Tony1324@users.noreply.github.com>
2020-04-23 08:45:35 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+window.addEventListener('load',() => {
+	var temp = 0;
+	var description = 0;
+	let long;
+	let lat;
+
+
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(position =>{
+
+		long = position.coords.longitude;
+		lat = position.coords.latitude;
+		
+		fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=2669340c1ffaa498ad124cd7d7bb6eba`)
+			.then(res => res.json())
+			.then(data => {
+				temp = data.main.temp;
+				description = data.weather[0].description;
+				document.getElementById('temp').innerHTML = Math.round((temp-273.1)*10)/10+"°C";
+				document.getElementById('description').innerHTML = description;
+			})
+		})	
+	}		
+});
